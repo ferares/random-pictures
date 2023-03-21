957b2f31c738a3d33736f08b16c92ef328458a3e
ferares <fermin@ares.uy>
2023-03-20 21:05:34 -0300

    Add webp file upload support Don't use lossess webp

--- src/middlewares/uploads.ts
+++ src/middlewares/uploads.ts
@@ -1,10 +1,10 @@
 import { Request, RequestHandler } from 'express'
-import multer, { FileFilterCallback } from 'multer'
+import { FileFilterCallback } from 'multer'
 import sharp from 'sharp'
 
 class Uploads {
   public static imageFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
-    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG)$/)) {
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|webp|WEBP)$/)) {
       return cb(new Error('Only image files are allowed!'))
     }
     cb(null, true)
@@ -16,7 +16,7 @@ class Uploads {
     const files = (req.files as Express.Multer.File[])
     files.map((file: Express.Multer.File) => {
       promises.push(
-        sharp(file.buffer).resize(1500).webp({ lossless: true }).toBuffer()
+        sharp(file.buffer).resize(1500).webp({}).toBuffer()
       )
     })
     Promise.all(promises).then((buffers) => {
